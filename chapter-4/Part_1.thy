2b5cd8ab0c914a318f5d14e1e184e7c5b89b0353
Joshua Morris <joshuajohnmorris@gmail.com>
2019-11-18 11:21:12 +1000

    forgot

--- chapter-4/Part_1.thy
+++ chapter-4/Part_1.thy
@@ -12,4 +12,24 @@ fun ord :: "int tree \<Rightarrow> bool" where
 "ord Tip = True" |
 "ord (Node left a right) = ((\<forall>x\<in>set left. x \<le> a) \<and> (\<forall>y\<in>set right. y > a) \<and> ord left \<and> ord right)"
 
+fun ins :: "int \<Rightarrow> int tree \<Rightarrow> int tree" where
+"ins n Tip = Node Tip n Tip" |
+"ins n (Node left a right) = 
+  (if n = a 
+    then Node left a right 
+  else if n \<le> a 
+    then Node (ins n left) a right
+  else
+    Node left a (ins n right))"
+
+lemma set_ins [simp] : "set (ins x t) = {x} \<union> set t"
+  apply(induction t)
+   apply(auto)
+  done
+
+lemma ord_ins : "ord t \<Longrightarrow> ord (ins i t)"
+  apply(induction t)
+   apply(auto)
+  done
+
 end
